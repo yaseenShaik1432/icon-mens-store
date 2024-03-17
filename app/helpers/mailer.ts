7461989dae457632ae8404765cbc75f842eed95a
yaseenShaik1432 <personal.shaiks@gmail.com>
2024-03-17 12:45:14 +0530

    spell change

--- app/helpers/mailer.ts
+++ app/helpers/mailer.ts
@@ -1,44 +1,53 @@
-import nodemailer from 'nodemailer';
-import bcryptjs from 'bcryptjs';
-import User from '../models/userModels';
+import nodemailer from "nodemailer";
+import bcryptjs from "bcryptjs";
+import User from "../models/userModels";
 
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    // create a Hashed token
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
 
-export const sendEmail = async({email, emailType, userId}:any) => {
-    try {
-        // create a hased token
-        const hashedToken = await bcryptjs.hash(userId.toString(), 10)
-
-        if (emailType === "VERIFY") {
-            await User.findByIdAndUpdate(userId, 
-                {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000})
-        } else if (emailType === "RESET"){
-            await User.findByIdAndUpdate(userId, 
-                {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000})
-        }
-
-        var transport = nodemailer.createTransport({
-            service: "Gmail",
-            auth: {
-              user: "personal.shaiks@gmail.com",
-              pass: "ebbsizfhcztomkbz",
-            },
-          });
-
-
-        const mailOptions = {
-            from: 'personal.shaiks@gmail.com',
-            to: email,
-            subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
-            html: `<p>Click <a href="${process.env.DOMAIN}/${emailType==="VERIFY" ? "verifyemail" : "reset-password"}?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
-            or copy and paste the link below in your browser. <br> ${process.env.DOMAIN}/${emailType==="VERIFY" ? "verifyemail" : "reset-password"}?token=${hashedToken}
-            </p>`
-        }
-
-        const mailresponse = await transport.sendMail
-        (mailOptions);
-        return mailresponse;
-
-    } catch (error:any) {
-        throw new Error(error.message);
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
     }
-}
+
+    var transport = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "personal.shaiks@gmail.com",
+        pass: "ebbsizfhcztomkbz",
+      },
+    });
+
+    const mailOptions = {
+      from: "personal.shaiks@gmail.com",
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verify your email" : "Reset your password",
+      html: `<p>Click <a href="${process.env.DOMAIN}/${
+        emailType === "VERIFY" ? "verifyemail" : "reset-password"
+      }?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+            or copy and paste the link below in your browser. <br> ${
+              process.env.DOMAIN
+            }/${
+        emailType === "VERIFY" ? "verifyemail" : "reset-password"
+      }?token=${hashedToken}
+            </p>`,
+    };
+
+    const mailresponse = await transport.sendMail(mailOptions);
+    return mailresponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
